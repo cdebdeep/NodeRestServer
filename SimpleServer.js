2ce76b6c1713e85e25b62ac94f6d83d50ba01fe1
Debdeep Chaudhuri <cdebdeep@Debdeeps-MacBook-Air.local>
2016-11-04 13:16:32 +0530

    Body-Parser & Http Post done

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,10 +1,31 @@
 "use strict";
 /// <reference path="typings/index.d.ts" />
 var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-    res.write('hello world');
-    res.end('hello');
+var myBodyParser = require("body-parser");
+var myData = [];
+myData.push({
+    'Name': 'Debdeep Chaudhuri',
+    'Age': 39,
+    'Sex': 'Male',
+    'DOB': '26/10/1977'
+}, {
+    'Name': 'Somdatta Chaudhuri',
+    'Age': 39,
+    'Sex': 'FeMale',
+    'DOB': '29/03/1977'
 });
-var server = app.listen(8081);
+var app = express();
+app.use(myBodyParser.json());
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.json(myData);
+    res.end();
+});
+router.post('/', function (req, res) {
+    myData.push(req.body);
+    res.json(myData);
+    res.end();
+});
+app.use('/api', router);
+var server = app.listen(8080);
 console.log(server.address().port);
--- SimpleServer.ts
+++ SimpleServer.ts
@@ -1,13 +1,46 @@
 /// <reference path="typings/index.d.ts" />
 import express = require('express')
 import http = require("http");
+import * as myBodyParser from "body-parser";
 
+var myData:Array<Object>=[];
+myData.push(
+    {
+     'Name':'Debdeep Chaudhuri',
+      'Age' :39,
+      'Sex':'Male',
+      'DOB' :'26/10/1977'
+    },
+    {
+     'Name':'Somdatta Chaudhuri',
+      'Age' :39,
+      'Sex':'FeMale',
+      'DOB' :'29/03/1977'
+    }
+      );
 
 var app  = express();
-app.get('/',function(req:http.ServerRequest,res:http.ServerResponse,){
-res.write('hello world');
-res.end();
-})
 
-var server =  app.listen(8081);
+app.use(myBodyParser.json());
+
+var router = express.Router();
+
+router.get('/',function(req:any,res:any){
+res.json(
+    myData
+);
+res.end();
+});
+
+router.post('/',function(req:any,res:any){
+myData.push(req.body);
+res.json(
+    myData
+);
+res.end();
+});
+
+
+app.use('/api',router);
+var server =  app.listen(8080);
 console.log(server.address().port)
